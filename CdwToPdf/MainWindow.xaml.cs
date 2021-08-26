c07ab09be6d51fcebf85c01dfff8cdbad315b337
Alexander Pachkovsky <darkb@outlook.com>
2021-08-26 16:57:46 +0300

    8/26/2021

--- CdwToPdf/MainWindow.xaml.cs
+++ CdwToPdf/MainWindow.xaml.cs
@@ -27,16 +27,20 @@ namespace CdwToPdf
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void BtnConvert_Click(object sender, RoutedEventArgs e)
         {
+            const string KOMPAS_PROG_ID = "KOMPAS.Application.7";
+            const string KOMPAS_PATH_PDF_CONVERTER = @"C:\Program Files\ASCON\KOMPAS-3D v20\Bin\Pdf2d.dll";
 
-            Type t7 = Type.GetTypeFromProgID("KOMPAS.Application.7") ?? throw new InvalidOperationException();
+            Type t7 = Type.GetTypeFromProgID(KOMPAS_PROG_ID) ?? throw new InvalidOperationException();
             IApplication kmpsApp = Activator.CreateInstance(t7) as IApplication ?? throw new InvalidOperationException();
 
+            
 
-            IConverter iConverter = kmpsApp.Converter[@"C:\Program Files\ASCON\KOMPAS-3D v20\Bin\Pdf2d.dll"];
+            IConverter iConverter = kmpsApp.Converter[KOMPAS_PATH_PDF_CONVERTER];
 
             IPdf2dParam param = (IPdf2dParam)iConverter.ConverterParameters(0);
+            param.OnlyThinLine = true;
 
             var result = iConverter.Convert(@"C:\Try\1.cdw", @"C:\Try\1.pdf", 0, false); 
 
