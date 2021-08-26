248106adcf0484b5adf4112495a7c0c8d485c17c
darkbgit <darkb@mail.ru>
2021-08-26 21:18:02 +0300

    8/26/2021 ev

--- CdwToPdf/MainWindow.xaml.cs
+++ CdwToPdf/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 using KompasAPI7;
 using Pdf2d_LIBRARY;
 
@@ -33,21 +34,37 @@ namespace CdwToPdf
             const string KOMPAS_PATH_PDF_CONVERTER = @"C:\Program Files\ASCON\KOMPAS-3D v20\Bin\Pdf2d.dll";
 
             Type t7 = Type.GetTypeFromProgID(KOMPAS_PROG_ID) ?? throw new InvalidOperationException();
-            IApplication kmpsApp = Activator.CreateInstance(t7) as IApplication ?? throw new InvalidOperationException();
+            IApplication kmpsApp =
+                Activator.CreateInstance(t7) as IApplication ?? throw new InvalidOperationException();
 
-            
+
 
             IConverter iConverter = kmpsApp.Converter[KOMPAS_PATH_PDF_CONVERTER];
 
             IPdf2dParam param = (IPdf2dParam)iConverter.ConverterParameters(0);
             param.OnlyThinLine = true;
 
-            var result = iConverter.Convert(@"C:\Try\1.cdw", @"C:\Try\1.pdf", 0, false); 
+            var result = iConverter.Convert(@"C:\Try\1.cdw", @"C:\Try\1.pdf", 0, false);
 
-            MessageBox.Show(result.ToString());
+            System.Windows.MessageBox.Show(result.ToString());
 
 
         }
 
+        private void BtnChoose_Click(object sender, RoutedEventArgs e)
+        {
+            string path;
+            using var dialog = new FolderBrowserDialog
+            {
+                Description = "Выберите папку",
+                UseDescriptionForTitle = true,
+                ShowNewFolderButton = true
+            };
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = dialog.SelectedPath;
+                System.Windows.MessageBox.Show(path);
+            }
+        }
     }
 }
